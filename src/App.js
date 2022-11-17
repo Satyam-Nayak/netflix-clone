0d48e8ad78d1216ba38293e51a0cea44bdd5965c
Satyam-Nayakm <satyamn120@gmail.com>
2022-11-17 19:09:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// !just create a function here
+
+import './App.css'
+const App = () => {
+  const Name = 'letsupgrade';
+
+  return (
+  <div>
+    <h1 className='title' >Hello {Name} </h1>
+    <p id= 'sub-title'>loream ipsum dolor sit amet, conserajd, slit, sonu satyam.</p>
+  </div>
+  );
+};
+
+export default App;
