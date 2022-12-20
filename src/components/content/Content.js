52029d22eaa0614af4783cea4df7ec4075ffe862
Satyam-Nayakm <satyamn120@gmail.com>
2022-12-20 08:56:50 +0530

    monday new

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,3 +1,4 @@
-export default function Content(){
-    return <div className='content-wrapper'>Content</div>
+import './Content.css';
+export default function Content() {
+    return <div className='content-wrapper'>Content</div>;
 }
